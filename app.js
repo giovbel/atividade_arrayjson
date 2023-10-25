bcb7bd73b680711e94a67ce22771816142350b74
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2023-10-25 11:18:41 -0300

    finalizado

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ jsonSiglas.quantidade = siglas.length;
     return jsonSiglas;
 }
 
+
 //--------------------------------------------------------------------------
 
 //função para aparecer os dados do estado através da sigla com filtro
@@ -60,4 +61,131 @@ const getDadosEstado = function (siglaFiltro){
 }
 
 console.log(getListaDeEstados())
-console.log(getDadosEstado("sp"))
+console.log(getDadosEstado("SP"))
+
+//--------------------------------------------------------------------------
+
+const getCapitalEstado = function (siglaFiltro){
+
+    let estados = estadosEcidades.estados;
+    let siglaDoEstado = siglaFiltro;
+    let jsonEstado = {}
+
+    estados.forEach(function (estado){
+
+        if(siglaDoEstado.toUpperCase() == estado.sigla.toUpperCase()){
+            jsonEstado.uf = estado.sigla;
+            jsonEstado.descricao = estado.nome;
+            jsonEstado.capital = estado.capital;
+        }
+    })
+
+    return jsonEstado;
+}
+
+console.log(getCapitalEstado("AC"))
+
+//--------------------------------------------------------------------------
+
+const getEstadosRegiao = function(regiao){
+
+    let regiaoEstado = regiao;
+    let estados = estadosEcidades.estados;
+    let jsonRegiao = {}
+
+    jsonRegiao.regiao = regiaoEstado;
+
+    let arrayEstado = []
+    jsonRegiao.estados = arrayEstado; 
+
+    estados.forEach(function (estado){
+
+        // criar uma variável JSON para os estados
+        let jsonEstado = {}
+
+        if(regiaoEstado.toUpperCase() == estado.regiao.toUpperCase()){
+
+            // colocar os ‘atributos’ dentro do JSON e definir um valor para eles
+            jsonEstado.uf = estado.sigla;
+            jsonEstado.descricao = estado.nome;
+
+            arrayEstado.push(jsonEstado)
+        }
+    })
+    
+    return jsonRegiao;
+}
+
+console.log(getEstadosRegiao("SUL"))
+
+//--------------------------------------------------------------------------
+
+const getCapitalPais = function(){
+
+    let estados = estadosEcidades.estados
+    let jsonCapitais = {}
+    let arrayCapitais = []
+
+    jsonCapitais.capitais = arrayCapitais;
+
+    estados.forEach(function (estado){
+
+        let jsonEstado = {}
+
+        if(estado.capital_pais){
+
+            jsonEstado.capital_atual = estado.capital_pais.capital
+            jsonEstado.uf = estado.sigla;
+            jsonEstado.descricao = estado.nome;
+            jsonEstado.capital = estado.capital
+            jsonEstado.regiao = estado.regiao;
+            jsonEstado.capital_pais_ano_inicio = estado.capital_pais.ano_inicio;
+            jsonEstado.capital_pais_ano_termino = estado.capital_pais.ano_fim;
+
+            arrayCapitais.push(jsonEstado)
+        }
+    })
+
+    return jsonCapitais;
+}
+
+console.log(getCapitalPais())
+
+//--------------------------------------------------------------------------
+
+const getCidades = function(siglaFiltro){
+
+    let estados = estadosEcidades.estados;
+    let siglaDoEstado = siglaFiltro;
+    let jsonEstado = {}
+    let arrayCidades = []
+
+
+    jsonEstado.uf = siglaDoEstado
+    
+    estados.forEach(function (estado){
+
+    if(siglaDoEstado.toUpperCase() == estado.sigla.toUpperCase()){
+
+        jsonEstado.descricao = estado.nome
+       
+        
+
+
+        estado.cidades.forEach(function(cidade){
+            arrayCidades.push(cidade.nome)
+        })
+
+
+        jsonEstado.quantidade_cidades = arrayCidades.length
+        jsonEstado.cidades = arrayCidades;
+    }
+
+    
+
+    })
+    
+    return jsonEstado
+}
+
+console.log(getCidades("AC"))
